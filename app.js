b0930e729d71d9601c856102443ec0eae007e337
stephane boguhe <boguhestephane97@gmail.com>
2022-04-05 10:17:59 +0000

    ajout de dotenv
    -> dotenv est un package permettant de lire les variables se trouvant dans notre env
    -> Il doit être installé

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
+
+//TODO : permet t'utiliser les varible stocker dan =s le .env
+//! notre APIKey y est déclaré.
+const dotenv = require("dotenv");
+dotenv.config();
+
 //Initialisation du projet express
 const app = express();
 
+//TODO : pour la manipulation des fichier .json et données reçus via la methode post...
+//! doit être installé
 const bodyParser = require("body-parser");
 const axios = require("axios");
+
+//TODO : Chargement du modele
 const Weather = require("./weather");
 
+//? important
+// TODO : On autorise notre application à utiliser bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -15,8 +29,7 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   const query = req.body.countryName;
   const unit = "metric";
-  const apiKey = process.env.API_KEY;
-
+  const apiKey = process.env.APIKey;
   //Recupère la ressource de nos données
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -45,7 +58,7 @@ app.post("/", (req, res) => {
           data.country +
           " </h1>"
       );
-      res.write("<p>La meteo est actuellemnt " + data.msg + "</p>");
+      res.write("<p>Temps " + data.msg + "</p>");
       res.write("<img src = " + imageUrl + " />");
 
       res.send();
@@ -55,6 +68,6 @@ app.post("/", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log("connexion au port 3000");
+app.listen(port, () => {
+  console.log("connexion au port " + port);
 });
